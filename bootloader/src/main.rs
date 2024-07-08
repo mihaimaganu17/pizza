19bcefabcb1c8ba3da5ca7ed80bd22a8fa7b1530
mihaimaganu17 <maganu_gx_mihai@yahoo.com>
2024-07-08 10:30:20 +0200

    Added a no_std, no_main rust entry point for the bootloader

--- bootloader/src/main.rs
+++ bootloader/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+#[link(name="c")]
+extern "C" {
+}
+
+#[no_mangle]
+fn main(_argc: isize, _argv: *const *const u8) -> i32 {
+    panic!("Freaking out")
+}
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
 }
