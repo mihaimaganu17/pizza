6c507bfe3b7bc288b0b91f3dd13a5a20ed6661b5
mihaimaganu17 <maganu_gx_mihai@yahoo.com>
2024-07-12 08:47:26 +0200

    Added memset implementation

--- bootloader/src/compiler_builtins.rs
+++ bootloader/src/compiler_builtins.rs
@@ -2,6 +2,13 @@
 /// C has different argument names.
 /// Returns the `dest` buffer
 #[no_mangle]
-pub extern "C" fn memset(dest: *mut u8, value: i32, len: usize) -> *mut u8 {
+pub unsafe extern "C" fn memset(dest: *mut u8, value: i32, len: usize) -> *mut u8 {
+    // TODO: We should check if the pointer is null. However, this is not C behavioud :D
+    let mut idx = 0;
+    while idx < len {
+        // *dest.offset can also be used
+        *dest.add(idx) = value as u8;
+        idx += 1;
+    }
     dest
 }
