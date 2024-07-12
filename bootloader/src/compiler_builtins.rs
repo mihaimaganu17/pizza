f06947505f6f6b6ef227ed292c5a9828c1c25216
mihaimaganu17 <maganu_gx_mihai@yahoo.com>
2024-07-12 09:06:17 +0200

    Added dummy mainCRTStartup

--- bootloader/src/compiler_builtins.rs
+++ bootloader/src/compiler_builtins.rs
@@ -12,3 +12,13 @@ pub unsafe extern "C" fn memset(dest: *mut u8, value: i32, len: usize) -> *mut u
     }
     dest
 }
+
+/// Dummy defined symbol for the entry point function of a windows binary which uses the
+/// /Subsystem:Console compiler environment.
+#[no_mangle]
+pub unsafe extern "C" fn mainCRTStartup() -> i32 {
+    // Notify the user that if it ever need this path of main execution, he has to implement it
+    panic!("No mainCRTStartup implementation");
+    // Typical C error code
+    -1
+}
