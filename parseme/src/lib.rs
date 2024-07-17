2f2bd0185a1fa71a32cc3189f29ad3894debbe6a
mihaimaganu17 <maganu_gx_mihai@yahoo.com>
2024-07-17 10:59:54 +0200

    Getting fields for build  trait implementations

--- parseme/src/lib.rs
+++ parseme/src/lib.rs
@@ -1,5 +1,6 @@
 use proc_macro::TokenStream;
 use quote::quote;
+use syn::{Data, Fields};
 
 #[proc_macro_derive(ReadMe)]
 pub fn derive(input: TokenStream) -> TokenStream {
@@ -8,6 +9,44 @@ pub fn derive(input: TokenStream) -> TokenStream {
 
     let name = input.ident;
 
+    match input.data {
+        Data::Struct(data_struct) => {
+            match data_struct.fields {
+                Fields::Named(fields) => {
+                    // Go through each field
+                    for field in fields.named {
+                        // Get the ident for the field
+                        let ident = if let Some(ident) = field.ident {
+                            ident
+                        } else {
+                            unimplemented!("Tuple fields are not implemented")
+                        };
+                        /*
+                        let type_ident = match field.ty {
+                            Type::Path(path) => {
+                                
+                            }
+                            _ => unimplemented!("Fields of other type than `Type:Path` are no supported")
+                        }
+                        */
+                        let type_ident = field.ty;
+                        let temp_tok = quote! {
+                            #[derive(Debug)]
+                            struct Builder {
+                                #ident: #type_ident
+                            }
+                        };
+                        return temp_tok.into();
+                        println!("{:#?}", temp_tok);
+                    }
+                }
+                _ => unimplemented!("Unamed and unit fields are not implemented")
+            }
+        }
+        _ => unimplemented!("Current procedural macro is not implemented for `Enum` and `Union`")
+    }
+
+    // Construct the `Primitive` trait implementation for this structure.
     let tokens = quote! {
         impl read_me::Primitive for #name {
             fn read(data: &[u8]) -> Result<Self, ReaderError> {
@@ -17,15 +56,6 @@ pub fn derive(input: TokenStream) -> TokenStream {
         }
     };
 
-
-    /*
-    match input.data {
-        Data(data_struct) => {
-        }
-    }
-    */
-
-    println!("{:#?}", input);
     tokens.into()
 }
 
