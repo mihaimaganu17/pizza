185f3fd583d3389a0dc14ca6cfe5a4cc2d2ce200
mihaimaganu17 <maganu_gx_mihai@yahoo.com>
2024-08-06 10:11:40 +0200

    Refactoring halt and new init for serial ports

--- bootloader/src/main.rs
+++ bootloader/src/main.rs
@@ -4,27 +4,13 @@
 mod compiler_builtins;
 
 use core::panic::PanicInfo;
-use core::arch::asm;
-
+use cpu::x86::halt;
+use serial::Serial;
 
 #[no_mangle]
 extern "C" fn entry() {
-    unsafe {
-        // https://wiki.osdev.org/Printing_To_Screen
-        let com_ptr: *const u16 = 0x0400 as *const u16;
-        for i in 0..4 {
-            let port: u16 = unsafe { com_ptr.offset(i).read() };
-            if port == 0 {
-                continue;
-            }
-            init_serial(port);
-        }
-        core::ptr::write(0xB8000 as *mut u16, 0x0f4d);
-        asm!(
-            "cli",
-            "hlt",
-        );
-    }
+    Serial::init();
+    halt();
 }
 
 #[panic_handler]
