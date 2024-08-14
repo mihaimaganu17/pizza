2cc602633d4fcfcf416cb5fd6f06cb521886c520
mihaimaganu17 <maganu_gx_mihai@yahoo.com>
2024-08-14 10:25:46 +0200

    Added nightly to support better panic message

--- bootloader/src/main.rs
+++ bootloader/src/main.rs
@@ -12,21 +12,20 @@ extern "C" fn entry() {
     Serial::init();
     print!("Hello world!\n");
     println!("Hello world2!\n");
-    panic!("An empty line above");
+    let mem = [b'M'; 1000];
+    print!("{}\n", mem[..][80+1000]);
     halt();
 }
 
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
+    // Print the location where the panic occurred
     if let Some(loc) = info.location() {
         println!("System panic: {}:{}", loc.file(), loc.line());
     } else {
         println!("System panic: unknown location");
     }
-    if let Some(p) = info.payload().downcast_ref::<&str>() {
-        println!("System panic: {p:?}");
-    } else {
-        println!("System panic: unknown reason");
-    }
+    // Print the message for the panic
+    println!("{:?}", info.message());
     halt()
 }
